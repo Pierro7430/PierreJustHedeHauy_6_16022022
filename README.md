48f8d60488d83ab0c383097e9cc42cb10f813109
Pierre-Just HEDE-HAUY <pjhauy@gmail.com>
2022-03-11 09:04:26 +0100

    fix put controller and README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+This repo Backend need repo frontend from P6 : https://github.com/OpenClassrooms-Student-Center/Web-Developer-P6
+
+## Install ##
+
+Here are the dependancies you need to install:
+- NodeJS
+
+Then, clone this repo, run `npm install`
+
+Change the values in the file .env_EXAMPLE and rename the file : .env
+
+## Usage ##
+
+readme :.env enxemple
+Run `nodemon server`
--- controllers/sauce.js
+++ controllers/sauce.js
@@ -32,16 +32,30 @@ exports.modifySauce = (req, res, next) => {
                 return
             }
 
-            const sauceObject = req.file ?
-            {
-                ...JSON.parse(req.body.sauce),
-                imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-            } : { ...req.body };
+           let sauceObject;
+
+            if (req.file) {
+                const filename = sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`,function(err){
+                    if(err) return console.log(err);
+                    console.log('file deleted successfully');
+                });
+
+                sauceObject = {
+                    ...JSON.parse(req.body.sauce),
+                    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+                }
+            }
+            else {
+                sauceObject = { ...req.body }
+            }
+
             Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
                 .then(() => res.status(200).json({ message: 'Objet modifié !'}))
                 .catch(error => res.status(400).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
+        
 };
 
 
